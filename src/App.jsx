3530ac06b0d58bccdfd17f4b2d49de3cf8638a78
kbmelo <kbmelo@outlook.com>
2020-11-23 11:20:14 -0300

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
+import { useRef } from 'react';
+
 import './App.css';
 
 const App = () => {
+  const count = useRef(0);
+  let count2 = 0;
+
+  const inputRef = useRef();
+
+  const buttonRef = useRef();
+
   return (
     <div className="App">
-      <h1>Oi</h1>
+      <h1>{count.current}</h1>
+
+      <input
+        type="text"
+        ref={inputRef}
+      />
+
+      <button onClick={() => {
+        inputRef.current.value += 'Valor';
+        console.log(inputRef.current);
+        
+        buttonRef.current.click();
+        inputRef.current.focus();
+        // count.current += 1;
+        // count2 += 1;
+
+        // console.log('count', count.current);
+        // console.log('count2', count2);
+      }}>Increment</button>
+
+      <button
+        onClick={() => alert('CLICOU')}
+        ref={buttonRef}  
+      >Alert</button>
     </div>
   );
-}
+};
 
 export default App;
